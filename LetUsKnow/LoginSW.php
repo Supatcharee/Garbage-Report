adeb9ab9e763df18b62650637c526fa68dba05a8
Supatcharee <pat-mol_2536@hotmail.com>
2015-03-17 17:46:03 +0700

    login service

---
+++ LetUsKnow/LoginSW.php
@@ -0,0 +1,51 @@
+<?php
+include("config_db.php");
+
+$passcode = $_GET["passcode"];
+
+
+$query = mysql_query("SELECT customer.passcode FROM customer WHERE passcode ='".$passcode."'");
+
+
+if($passcode == null){
+	echo "No data";
+}
+
+else{
+	if ($value = mysql_num_rows($query) > 0) {
+	
+	while($e = mysql_fetch_assoc($query)){
+		
+	$output[]=$e;
+	}
+
+	
+}
+
+print(json_encode($output));
+
+}
+
+
+	
+
+
+/*$intNumRows = mysql_num_rows($query);
+
+
+if ($intNumRows > 0) {
+	
+	echo "pass";
+
+}else {
+	echo "loginfail";
+}
+
+echo (json_encode($query));*/
+
+
+
+?> 
+
+
+
